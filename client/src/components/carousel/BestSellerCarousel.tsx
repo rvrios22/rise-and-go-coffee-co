00405b7a21f393e7e722abaf45c871eca40cdb9a
Rafael Rios <rvrios22@gmail.com>
2025-02-03 05:53:50 -0800

    add Carousel

--- client/src/components/carousel/BestSellerCarousel.tsx
+++ client/src/components/carousel/BestSellerCarousel.tsx
@@ -1,5 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { TopSeller } from "../../types/topSeller";
+import CarouselItem from "./CarouselItem";
+import "../../css/carousel.css";
+import { useCallback } from "react";
+
 const getTopSellers = async () => {
   const response = await fetch("/api/topSeller");
   if (!response.ok) {
@@ -10,10 +14,23 @@ const getTopSellers = async () => {
 };
 
 function BestSellerCarousel() {
+  const carouselRef = useCallback((carousel: HTMLDivElement) => {
+    const cloneItems = () => {
+      const items = Array.from(carousel.children) as HTMLDivElement[];
+
+      items.forEach((item) => {
+        const clone = item.cloneNode(true) as HTMLDivElement;
+        carousel.appendChild(clone);
+      });
+    };
+
+    cloneItems();
+  }, []);
   const { isPending, isError, data, error } = useQuery({
     queryKey: ["topSelers"],
     queryFn: getTopSellers,
   });
+
   if (isPending) {
     return <span>Loading...</span>;
   }
@@ -22,15 +39,19 @@ function BestSellerCarousel() {
     return <span>Error: {error.message}</span>;
   }
 
-  // We can assume by this point that `isSuccess === true`
   return (
-    <ul>
-      {data.map((item: TopSeller) => (
-        <li key={item.id}>
-          {item.name} ${item.amount}
-        </li>
-      ))}
-    </ul>
+    <figure className="carousel-container">
+      <div ref={carouselRef} className="carousel-track">
+        {data.map((item: TopSeller) => (
+          <CarouselItem
+            key={item.id}
+            name={item.name}
+            id={item.id}
+            amount={item.amount}
+          />
+        ))}
+      </div>
+    </figure>
   );
 }
 
--- client/src/components/carousel/CarouselItem.tsx
+++ client/src/components/carousel/CarouselItem.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { TopSeller } from "../../types/topSeller";
+import Image from "../Image";
 
-function CarouselItem() {
+function CarouselItem({ name, amount }: TopSeller) {
   return (
-    <div>CarouselItem</div>
-  )
+    <article>
+      {/* <img
+        src="logo.webp"
+        alt=""
+        height={window.innerHeight / 6}
+        width={window.innerWidth / 3}
+      /> */}
+      <Image
+        height={window.innerHeight / 6}
+        width={window.innerWidth / 3}
+        name=""
+        alt={name}
+      />
+      <p className="lato">{name}</p>
+      <p className="lato">${amount}</p>
+    </article>
+  );
 }
 
-export default CarouselItem
+export default CarouselItem;
